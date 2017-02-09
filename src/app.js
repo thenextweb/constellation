bf2b6d6b62a7d5bc56370db0f3ddaae63f089d94
Laura González <lawr@lauragonzalez.cc>
2017-02-09 11:10:10 +0100

    minor performance updates

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const Puppis = require("worker-loader?inline!./worker/puppis.js");
 
+const styleDefaults = {
+	starSize: 4,
+	starPadding: 5,
+	starColor: '#000',
+	lineColor: 'rgba(0,0,0,.5)',
+	lineSize: 2
+};
+
 const constellation = function ({
 	size = [400,400],
 	element = undefined,
 	canvas = undefined,
-	nodesTotal = 30,
+	starsTotal = 30,
 	shipsTotal = 70,
 	fuzziness = 100,
 	padding = [0,0],
@@ -17,30 +25,24 @@ const constellation = function ({
 	onDraw = {}
 } = {}) {
 
-	const puppis = new Puppis();
 
-	if(padding[0] === 0 && padding[1] === 0) {
-		padding = [fuzziness,fuzziness]
-	}
-
-	const styleDefaults = {
-		starSize: 4,
-		starPadding: 5,
-		starColor: '#000',
-		lineColor: 'rgba(0,0,0,.5)',
-		lineSize: 2
-	};
-
+	/*add to defaults*/
+	if(padding[0] === 0 && padding[1] === 0) padding = [fuzziness,fuzziness]
 	style = Object.assign({}, styleDefaults, style);
 
+
+	/*set up parameters*/
 	let jiggles = true;
 	let lastMouse = [0,0];
 
+
+	/*send all the important bits to worker*/
+	const puppis = new Puppis();
 	puppis.postMessage({
 		body: 'sendParameters',
 		payload: {
 			speed: speed,
-			nodesTotal: nodesTotal,
+			starsTotal: starsTotal,
 			shipsTotal: shipsTotal,
 			padding: padding,
 			size: size,
@@ -49,45 +51,51 @@ const constellation = function ({
 		}
 	});
 
+
+	/*magic*/
 	const drawCanvas = (canvas,objects) => {
 
 		const ctx = canvas.getContext('2d');
 		ctx.clearRect(0, 0, size[0],size[1]);
 
 		/*lines*/
-		objects.lines.map((line)=>{
+		ctx.lineWidth = style.lineSize;
+		ctx.strokeStyle = style.lineColor;
+		for (let i = 0, len = objects.lines.length; i < len; i++) {
+			let line = objects.lines[i]
 			if(onDraw.line) {
 				onDraw.line(ctx,style,line);
 			}
 			else {
 				ctx.beginPath();
-					ctx.lineWidth = style.lineSize;
-					ctx.strokeStyle = style.lineColor;
 					ctx.moveTo(line.pos[0],line.pos[1]);
 					ctx.lineTo(line.pos[2],line.pos[3]);
 					ctx.globalCompositeOperation = 'source-over';
 					ctx.stroke();
-				ctx.closePath();
+					ctx.closePath();
 			}
 			if(onDraw.afterLine) onDraw.afterLine(ctx,style,line);
-		});
+		}
 
 		/*stars*/
+		ctx.fillStyle = '#f0f';
 		if(style.starPadding > 0) {
 			ctx.globalCompositeOperation = 'destination-out';
-			objects.nodes.map((node)=>{
+			for (let i = 0, len = objects.nodes.length; i < len; i++) {
+				let node = objects.nodes[i]
 				ctx.beginPath();
-					ctx.fillStyle = '#f0f';
 					ctx.arc(
 						node.pos[0], node.pos[1],
 						(style.starSize + style.starPadding),
 						0, 2 * Math.PI);
 					ctx.fill();
 				ctx.closePath();
-			});
+			}
 		}
 
-		objects.nodes.map((node)=>{
+		ctx.fillStyle = style.starColor;
+		for (let i = 0, len = objects.nodes.length; i < len; i++) {
+			let node = objects.nodes[i]
 			if(onDraw.star) {
 				onDraw.star(ctx,style,node);
 			}
@@ -96,20 +104,21 @@ const constellation = function ({
 					ctx.arc(
 						node.pos[0], node.pos[1], style.starSize,0, 2 * Math.PI
 					);
-					ctx.fillStyle = style.starColor;
 					ctx.globalCompositeOperation = 'source-over';
 					ctx.fill();
 				ctx.closePath();
 			}
 			if(onDraw.afterStar) onDraw.afterStar(ctx,style,node);
-		});
+		};
 		ctx.closePath();
 
 		if(onDraw.afterFrame) onDraw.afterFrame(ctx,style);
 
 	}
 
+
 	return new Promise((resolve,reject)=>{
+
 		let start = () => {
 
 			if(canvas) {
@@ -160,11 +169,13 @@ const constellation = function ({
 			});
 
 		};
+
 		if (/comp|inter|loaded/.test(document.readyState)){
 			start();
 		} else {
-			window.document.addEventListener("DOMContentLoaded",start);
+			window.document.addEventListener('DOMContentLoaded',start);
 		}
+
 	});
 
 };
